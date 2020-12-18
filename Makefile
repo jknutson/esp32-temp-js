821d4896e721dd49d199f5dae8db82c4e0213b54
John Knutson <john.m.knutson@gmail.com>
2020-12-18 14:38:42 -0600

    add multi-sensor support (#6)
    * mqtt updates
    
    * add todo
    
    * multi sensor working
    
    * fixup voltage
    
    * change source for temp sensor to include device id/address
    
    * remove unused c file
    
    * makefile update
    
    * tidying up
    
    * more cleanup

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ MOS=mos
 .PHONY: all build flash configure reboot
 
 build:
-	$(MOS) build
+	$(MOS) build --platform esp32
 
 flash:
 	$(MOS) flash
@@ -14,24 +14,32 @@ wifi:
 wifi-nsm2:
 	$(MOS) wifi "nsm2" "${WIFI_PASS}"
 
-configure:
+configure-datadog:
 	$(MOS) put combined.pem ca.pem
 	$(MOS) config-set datadog.api_key="${DD_API_KEY}" \
 		datadog.host_name="${DD_HOSTNAME}" \
-		pins.voltage="${PIN_VOLTAGE}" \
+
+configure-voltage:
+	$(MOS) pins.voltage="${PIN_VOLTAGE}" \
 		pins.voltage_r1="${PIN_VOLTAGE_R1}" \
 		pins.voltage_r2="${PIN_VOLTAGE_R2}" \
-		ds18b20.pin="${PIN_DS18B20}"
+
 
 configure-i2c:
 	$(MOS) config-set i2c.sda_gpio="${PIN_I2C_SDA}" \
 		i2c.scl_gpio="${PIN_I2C_SCL}"
 
 configure-mdash:
-	 $(MOS) config-set dash.enable=true dash.token="${MDASH_TOKEN}" mdash.device_id="${DD_HOSTNAME}"
+	 $(MOS) config-set dash.enable=true dash.token="${MDASH_TOKEN}"
+
+configure-mqtt:
+	$(MOS) config-set mqtt.enable=true mqtt.server=192.168.2.6
 
 reboot:
 	$(MOS) call Sys.Reboot
 
 all: build flash wifi configure reboot
-all-solar: build flash wifi-nsm2 configure configure-i2c configure-mdash reboot
+all-solar: build flash wifi-nsm2 configure configure-voltage configure-i2c configure-mdash reboot
+
+configure-compost: wifi-nsm2 configure configure-mdash
+all-compost: build flash configure-compost reboot
--- fs/init.js
+++ fs/init.js
@@ -1,22 +1,17 @@
 load('api_adc.js');
 load('api_config.js');
-load('api_events.js');
-load('api_gpio.js');
-load('api_http.js');
-load('api_net.js');
 load('api_timer.js');
-load('api_sys.js');
-load('api_ds18b20.js');
+load('api_arduino_onewire.js');
 load('api_lcd_i2c.js');
+load('api_mqtt.js');
+load('ds18b20.js');
 
 let deviceId = Cfg.get('device.id');
 let deviceType = 'esp32';
-let metricTags = ['device:' + deviceId, 'deviceType:' + deviceType];
 let buttonPin = Cfg.get('pins.builtin');  // builtin
 let voltagePin = Cfg.get('pins.voltage');
+let ds18b20Pin = Cfg.get('pins.ds18b20');
 let pollInterval = Cfg.get('interval') * 1000;
-let datadogApiKey = Cfg.get('datadog.api_key');
-let datadogHostName = Cfg.get('datadog.host_name');
 let lcdAddr = 0x27;
 let lcdNumRows = 4;
 let lcdNumCols = 20;
@@ -24,10 +19,44 @@ let lcdNumCols = 20;
 let r1 = Cfg.get('pins.voltage_r1'); // r1 of voltage divider (ohm)
 let r2 = Cfg.get('pins.voltage_r2'); // r2 of voltage divider (ohm)
 
+// setup onewire/ds18b20
+// Initialize OneWire library
+let ow = OneWire.create(ds18b20Pin);
+
+let adcReadVoltage = ffi('int mgos_adc_read_voltage(int)');
+
+// Number of sensors found on the 1-Wire bus
+let n = 0;
+// Sensors addresses
+let rom = ['01234567'];
+
+// Search for sensors
+let searchSens = function() {
+  let i = 0;
+  // Setup the search to find the device type on the next call
+  // to search() if it is present.
+  ow.target_search(DEVICE_FAMILY.DS18B20);
+
+  while (ow.search(rom[i], 0/* Normal search mode */) === 1) {
+    // If no devices of the desired family are currently on the bus, 
+    // then another type will be found. We should check it.
+    if (rom[i][0].charCodeAt(0) !== DEVICE_FAMILY.DS18B20) {
+      break;
+    }
+    // Sensor found
+    print('Sensor#', i, 'address:', toHexStr(rom[i]));
+    rom[++i] = '01234567';
+  }
+  return i;
+};
+
 if (voltagePin !== "" && r1 > 0 && r2 > 0) {
   print('voltage reading enabled');
   ADC.enable(voltagePin);
 }
+
+// setup mqtt last will
+Cfg.set({mqtt: {will_topic: 'esp32/' + deviceId + '/status', will_message: 'mqtt last will message'}});
 
 // setup LCD
 print('setting up lcd');
@@ -35,134 +64,61 @@ let lcd = LCD_I2C.create(lcdAddr);
 lcd.begin(lcdNumRows, lcdNumCols);
 lcd.clear();
 lcd.setCursor(0, 0);
-lcd.print('starting up...')
+lcd.print('starting up...');
 print('lcd setup complete');
 // lcd.setCursor(0, 1);
 // lcd.print('second line');
 
-print('deviceId:', deviceId)
-
-let postMetric = function(datadogApiKey, payload) {
-  print('publishing: ' + JSON.stringify(payload))
-  HTTP.query({
-    url: 'https://api.datadoghq.com/api/v1/series?api_key=' + datadogApiKey,
-    data: payload,
-    success: function(body, full_http_msg) {
-      print('datadog post metric success:', body);
-    },
-    error: function(err) {
-      print('datadog post metric error:', err);
-    }
-  });
-};
+print('deviceId:', deviceId);
 
 let multiplyVoltage = function(rawVoltage, r1, r2) {
   return (rawVoltage * (r1 + r2) / r2);
 };
 
-GPIO.set_button_handler(buttonPin, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 50, function(x) {
-  let buttonPinString = JSON.stringify(buttonPin);
-  // TODO: list sensors and include in event payload, plus any other debugging info?
-  let payload = JSON.stringify({
-    text: 'button on pin ' + buttonPinString + ' was pressed',
-    title: 'esp32 device button pressed',
-    host: datadogHostName,
-    tags: [
-      'device:' + deviceId,
-      'deviceType:' + deviceType,
-      'buttonPin:' + buttonPinString
-    ]
-  });
-
-  HTTP.query({
-    url: 'https://api.datadoghq.com/api/v1/events?api_key=' + datadogApiKey,
-    data: payload,
-    success: function(body, full_http_msg) {
-      print('datadog post event success:', body);
-    },
-    error: function(err) {
-      print('datadog post event error:', err);
-    }
-  });
-}, null);
-
-
 Timer.set(pollInterval, true, function() {
   let now = Timer.now();
 
-  // read system stats
-  let totalRam = Sys.total_ram();
-  let freeRam = Sys.free_ram();
-  let sysPayload = {
-    series: [
-      {
-        metric: 'mos.sys.total_ram',
-        points: [[now, totalRam]],
-        host: datadogHostName,
-        tags: metricTags,
-        type: 'gauge'
-      },
-      {
-        metric: 'mos.sys.free_ram',
-        points: [[now, freeRam]],
-        host: datadogHostName,
-        tags: metricTags,
-        type: 'gauge'
-      }
-    ]
-  };
-  print('publishing: ' + JSON.stringify(sysPayload))
-  postMetric(datadogApiKey, sysPayload);
+  // TODO: use sample library, collect a number of frequent samples and emit min, max, mean, etc.
 
   // read temperature
   let temperature = '???';
-  if (DS18B20.connected()) {
-    let t = DS18B20.get();
+  if (n === 0) {
+    if ((n = searchSens()) === 0) {
+      print('no DS18B20 device(s) found');
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    let t = getTemp(ow, rom[i]);
+    let tempSensorId = toHexStr(rom[i]);
     if (isNaN(t)) {
-      print('could not read from device: ' + t);
+      print('No device found');
       break;
     } else {
-      let payload = {
-        series: [
-          {
-            metric: 'w1_temperature.celcius.gauge',
-            points: [[Timer.now(), t]],
-            host: datadogHostName,
-            tags: metricTags,
-            type: 'gauge'
-          }
-        ]
-      };
-      print('publishing: ' + JSON.stringify(payload))
-      postMetric(datadogApiKey, payload);
-      temperature = t;
+      print('temperature (c): ' + JSON.stringify(t));
+      let t_f = (t * 1.8) + 32;
+      print('temperature (f): ' + JSON.stringify(t_f));
+      let topic = 'esp32/' + deviceId + '-' + tempSensorId + '/temperature';
+      print('topic: ', topic);
+      let res = MQTT.pub(topic, JSON.stringify(t_f));
+      print('MQTT Published (polled temperature):', res ? 'yes' : 'no');
+      temperature = t_f;
     }
-  } else {
-    print('no oneWire device found')
   }
 
+  // read voltage
   let voltage = '???';
-
   if (voltagePin !== "" && r1 > 0 && r2 > 0) {
-    // read voltage
-    let adcReadVoltage = ffi('int mgos_adc_read_voltage(int)');
     voltage = adcReadVoltage(voltagePin);
-    print('voltage: ', voltage);
-    let voltagePayload = {
-      series: [
-        {
-          metric: 'mos.voltage',
-          points: [[now, multiplyVoltage(voltage, r1, r2)]],
-          host: datadogHostName,
-          tags: metricTags,
-          type: 'gauge'
-        }
-      ]
-    };
-    print('publishing: ' + JSON.stringify(voltagePayload))
-    postMetric(datadogApiKey, voltagePayload);
+    print('raw voltage: ', voltage, 'mV');
+    let realVoltage = multiplyVoltage(voltage, r1, r2);
+    print('real voltage: ', realVoltage, 'mV');
+    let topic = 'esp32/' + deviceId + '/voltage/pin' + JSON.stringify(voltagePin);
+    print('topic: ', topic);
+    let res = MQTT.pub(topic, JSON.stringify(realVoltage));
+    print('MQTT Published (polled voltage):', res ? 'yes' : 'no');
   }
 
+  // redraw lcd
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print('v:' + JSON.stringify(voltage) + 'mV');
