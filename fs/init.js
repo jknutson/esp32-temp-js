b3d631dafae8156228d9f7fa7b57d3e862984d06
John M. Knutson <john.knutson@novu.com>
2021-07-12 22:01:16 -0500

    updates, but its too big

--- fs/init.js
+++ fs/init.js
@@ -8,8 +8,8 @@ load('api_timer.js');
 load('ds18b20.js');
 
 let deviceId = Cfg.get('device.id');
-let deviceType = 'esp32';
-let buttonPin = Cfg.get('pins.builtin');  // builtin
+print('deviceId:', deviceId);
+let buttonPin = Cfg.get('pins.builtin');
 let voltagePin = Cfg.get('pins.voltage');
 let ds18b20Pin = Cfg.get('pins.ds18b20');
 let pollInterval = Cfg.get('interval') * 1000;
@@ -19,30 +19,31 @@ let lcdNumCols = 20;
 // planning for future multi-sensor support
 let tempSensorId = 'ds18b20';
 
+// MQTT
+let mqTopicBase = 'iot/' + deviceId;
+Cfg.set({mqtt: {will_topic: mqTopicBase + '/status', will_message: 'mqtt last will message'}});
+
+// voltage reading
+let adcReadVoltage = ffi('int mgos_adc_read_voltage(int)');
 let r1 = Cfg.get('pins.voltage_r1'); // r1 of voltage divider (ohm)
 let r2 = Cfg.get('pins.voltage_r2'); // r2 of voltage divider (ohm)
+if (voltagePin !== "" && r1 > 0 && r2 > 0) {
+  print('voltage reading enabled');
+  ADC.enable(voltagePin);
+}
+let multiplyVoltage = function(rawVoltage, r1, r2) {
+  return (rawVoltage * (r1 + r2) / r2);
+};
 
-// setup onewire/ds18b20
-// Initialize OneWire library
+// onewire/ds18b20
 let ow = OneWire.create(ds18b20Pin);
-
-let adcReadVoltage = ffi('int mgos_adc_read_voltage(int)');
-
-// Number of sensors found on the 1-Wire bus
 let n = 0;
-// Sensors addresses
 let rom = ['01234567'];
-
-// Search for sensors
 let searchSens = function() {
   let i = 0;
-  // Setup the search to find the device type on the next call
-  // to search() if it is present.
   ow.target_search(DEVICE_FAMILY.DS18B20);
 
   while (ow.search(rom[i], 0/* Normal search mode */) === 1) {
-    // If no devices of the desired family are currently on the bus, 
-    // then another type will be found. We should check it.
     if (rom[i][0].charCodeAt(0) !== DEVICE_FAMILY.DS18B20) {
       break;
     }
@@ -52,14 +53,6 @@ let searchSens = function() {
   }
   return i;
 };
-
-if (voltagePin !== "" && r1 > 0 && r2 > 0) {
-  print('voltage reading enabled');
-  ADC.enable(voltagePin);
-}
-
-// setup mqtt last will
-Cfg.set({mqtt: {will_topic: 'esp32/' + deviceId + '/status', will_message: 'mqtt last will message'}});
 
 // setup LCD
 print('setting up lcd');
@@ -72,15 +65,9 @@ print('lcd setup complete');
 // lcd.setCursor(0, 1);
 // lcd.print('second line');
 
-print('deviceId:', deviceId);
-
-let multiplyVoltage = function(rawVoltage, r1, r2) {
-  return (rawVoltage * (r1 + r2) / r2);
-};
-
 GPIO.set_button_handler(buttonPin, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 50, function(x) {
   // builtin button pressed
-  let res = MQTT.pub('esp32/' + deviceId + '/events/buttonPushed', buttonPin);
+  let res = MQTT.pub(mqTopicBase + '/events/buttonPushed', buttonPin);
   print('MQTT Published (button pushed):', res ? 'yes' : 'no');
 }, null);
 
@@ -105,7 +92,7 @@ Timer.set(pollInterval, true, function() {
     } else {
       let t_f = (t * 1.8) + 32;
       print('temperature (c): ' + JSON.stringify(t));
-      let res = MQTT.pub('esp32/' + deviceId + '/temperature/' + tempSensorId, JSON.stringify(t_f));
+      let res = MQTT.pub(mqTopicBase + '/temperature/' + tempSensorId, JSON.stringify(t_f));
       print('MQTT Published (polled temperature):', res ? 'yes' : 'no');
       temperature = t_f;
     }
@@ -117,7 +104,7 @@ Timer.set(pollInterval, true, function() {
     let adcReadVoltage = ffi('int mgos_adc_read_voltage(int)');
     voltage = adcReadVoltage(voltagePin);
     print('voltage: ', voltage);
-    let res = MQTT.pub('esp32/' + deviceId + '/voltage/pin' + voltagePin, JSON.stringify(voltage));
+    let res = MQTT.pub(mqTopicBase + '/voltage/pin' + voltagePin, JSON.stringify(voltage));
     print('MQTT Published (polled voltage):', res ? 'yes' : 'no');
   }
 
